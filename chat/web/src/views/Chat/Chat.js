15b38629d187f5ddfa9277aff0de00f6a74f272c
Gabriel Martins <gabrielvegaz19@hotmail.com>
2022-05-25 01:11:46 -0300

    localhost to ip change

--- chat/web/src/views/Chat/Chat.js
+++ chat/web/src/views/Chat/Chat.js
@@ -57,7 +57,7 @@ const Chat = () => {
                   width="560"
                   height="315"
                   title="YouTube video player"
-                  frameBorder="2"
+                  frameBorder="0"
                   allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                   allowFullScreen
                 ></iframe>
